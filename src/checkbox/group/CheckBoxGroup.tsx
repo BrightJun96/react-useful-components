62b2a19023b73a0b4b6239555c1f51313f5cf26f
김철준 <jjalseu@gimcheoljun-ui-MacBookPro.local>
2024-01-18 01:13:45 +0900

    fix : vite.config.ts 수정

--- src/checkbox/group/CheckBoxGroup.tsx
+++ src/checkbox/group/CheckBoxGroup.tsx
@@ -3,18 +3,13 @@ import useGroupCheckOption from "../useGroupCheckOption";
 import useGroupCheckParentState from "../useGroupCheckParentState";
 import EntireCheck from "./EntireCheck";
 
-const mockInitOptions = [
-  { label: "치킨", value: "chicken", checked: false },
-  { label: "피자", value: "pizza", checked: false },
-  { label: "밥", value: "rice", checked: false },
-];
 /**
  * @description
  * 여러개 체크박스를 관리하는 컴포넌트입니다.
  * @param initOptions 초기 옵션입니다.
  */
 const CheckBoxGroup = <T,>({
-  initOptions = mockInitOptions,
+  initOptions = [],
   stateKey,
   setState,
   stateType,
--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,25 @@ export default defineConfig({
       name: "react-useful-common-components",
       fileName: "react-useful-common-components",
     },
-    // rollupOptions: {
-    //   external: ["react"],
-    //   output: {
-    //     globals: {
-    //       react: "React",
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      /**
+       * @description
+       * external: ["react", "react-dom"] 부분은 Rollup에게 "react"와 "react-dom"은 이 패키지 내부에서 직접 관리하지 않고, 패키지를 사용하는 프로젝트에서 제공할 것이라고 알려줍니다.
+       *  즉, 이 패키지의 번들에는 "react"와 "react-dom"이 포함되지 않습니다.
+       */
+      external: ["react", "react-dom"],
+      /**
+       * @description
+       * output: { globals: { react: "React", "react-dom": "ReactDOM" } } 부분은 빌드된 결과물에서 "react"와 "react-dom"이 전역 변수 "React"와 "ReactDOM"을 참조하도록 지시합니다.
+       * 이는 외부에서 제공되는 "react"와 "react-dom" 인스턴스를 사용하도록 합니다.
+       */
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
     // commonjsOptions: {
     //   esmExternals: ["react"],
     // },
